e2bc76d37c33cdb996eb589038023776b71bf4dc
Ruaraidh-Nicolson <98850567+Ruaraidh-Nicolson@users.noreply.github.com>
2022-02-08 17:19:57 +0000

    Added a new puzzle & readme file

--- World.hs
+++ World.hs
@@ -64,6 +64,7 @@ key       = Obj "key" "A metal key" "A key to the front door, why was it in the 
 mask      = Obj "mask" "A cloth mask" "A mask to prevent diseases"
 wallet    = Obj "wallet" "A leather wallet" "A wallet with money in it"
 matric    = Obj "matriculation" "A plastic card" "Matriculation card to get into buildings"
+mazemap   = Obj "map" "A small worn piece of paper" "A map of the backrooms. A world if you enter you may not leave. A key is drawn at the center of it. To get to the center go north, north, east, south, west, north, south"
 
 bedroom, kitchen, hall, street, living, garden :: Room
 
@@ -87,13 +88,60 @@ hall = Room "You are in the hallway. The front door is closed. "
              Exit "south" "To the south is a garden. " "garden"]
             []
 living = Room "You are in the living room." 
-         [Exit "east" "To the east is a hallway. " "hall"]
+         [Exit "east" "To the east is a hallway. " "hall",
+          Exit "south" "To the south is ???" "maze0"]
          [mask,matric]
 
 garden = Room "You are in the garden."
          [Exit "north" "To the north is a hallway. " "hall"]
-         [key]
+         [mazemap]
 
+-- Maze rooms all lead back to each original if strayed from the path.
+
+maze0 = Room "You have entered the beginning of the backrooms, you have noclipped out of reality, good luck :)"
+         [Exit "south" "To the south is the living room, you could clip back to reality if you're quick. " "living",
+          Exit "north" "To the north is a hallway. " "maze1",
+          Exit "east" "To the east is a hallway with a dark handprint on it. " "maze0"]
+         []        
+
+maze1 = Room "You are in a yellow hallway"
+         [Exit "north" "To the north is a yellow hallway. " "maze2",
+          Exit "east" "To the east is a blue hallway. " "maze0",
+          Exit "south" "To the south there is no longer a hallway. " "maze0"]
+         [] 
+
+maze2 = Room "You are in a yellow hallway"
+         [Exit "north" "To the north is a green hallway. " "maze0",
+          Exit "east" "To the east is a hallway of mirrors. " "maze3",
+          Exit "south" "To the south there is maybe a hallway, if you're lucky. " "maze0"]
+         [] 
+
+maze3 = Room "You are in a hallway?"
+         [Exit "north" "To the north is a hallway. " "maze0",
+          Exit "south" "To the south is a @^1!? hallway." "maze4"]
+         [] 
+
+maze4 = Room "a hallway are You in"
+         [Exit "north" "To the north is a yellow hallway. " "maze0",
+          Exit "east" "To the east is a hallway. " "maze0",
+          Exit "west" "To the west there is a hallway FINALLY. " "maze5"]
+         [] 
+
+maze5 = Room "You are in the best hallway"
+         [Exit "north" "To the north is a spiral hallway. " "maze6",
+          Exit "east" "To the east is a ladder. " "maze0",
+          Exit "south" "To the south there is maybe a hallway? " "maze0"]
+         [] 
+
+maze6 = Room "You are in a yellow hallway"
+         [Exit "north" "To the north is a hallway with a shape at the end of it. " "maze0",
+          Exit "east" "To the east is a hallway. " "maze0",
+          Exit "south" "To the south there is the way you came. It is n o r m a l. " "mazec"]
+         [] 
+
+mazec = Room "You have reached the center of the backrooms, congratulations, now leave."
+         [Exit "north" "To the north is a doorway to seemingly nowhere. " "living"]
+         [key] 
 
 -- New data about the hall for when we open the door
 
@@ -111,7 +159,15 @@ gameworld = [("bedroom", bedroom),
              ("street", street),
              ("living", living),
              ("garden",garden),
-             ("pantry",pantry)]
+             ("pantry",pantry),
+             ("maze0",maze0),
+             ("maze1",maze1),
+             ("maze2",maze2),
+             ("maze3",maze3),
+             ("maze4",maze4),
+             ("maze5",maze5),
+             ("maze6",maze6),
+             ("mazec",mazec)]
 
 initState :: GameData
 initState = GameData "bedroom" gameworld [] False False False
